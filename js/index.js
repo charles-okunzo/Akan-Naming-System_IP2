610a6457fb47e72ac7d48c8c8163511b7a470389
charles okunzo <okunzopeter01@gmail.com>
2022-03-05 09:56:21 +0300

    feature:submit button-add functionality to the submit button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const home=document.getElementById("home");
+const homeBtn=document.getElementById("home");
 
 const welcome=document.getElementById("welcome");
 
@@ -8,7 +8,9 @@ const myOutput=document.getElementById("myResult");
 
 const goBtn=document.getElementById("goBtn");
 
-home.onclick=()=>{
+const submitBtn= document.getElementById("submitBtn");
+
+homeBtn.onclick=()=>{
   welcome.style.display="flex";
   myForm.style.display="none";
   myOutput.style.display="none";
@@ -18,3 +20,11 @@ goBtn.onclick=()=>{
   welcome.style.display="none";
   myForm.style.display="flex";
 };
+
+submitBtn.addEventListener("click", (e)=>{
+  e.preventDefault();
+
+  myOutput.style.display="flex";
+  welcome.style.display="none";
+  myForm.style.display="none";
+})
